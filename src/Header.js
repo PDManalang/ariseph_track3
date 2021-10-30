2921770d0bb509b8cad75f3941625bce2bc0badf
yann <ayen@elliott.local>
2021-10-30 22:57:04 +0800

    updated navbar css

--- src/Header.js
+++ src/Header.js
@@ -1,35 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from "react-router-dom";
 
+import Dropdown from './Dropdown';
 import './Header.css'
 
 const Header = () => {
-    return(
-        <div className="header">
-            <div className="header_left">
-                <img src="https://www.arise.ph/wp-content/uploads/2021/10/arise-new-logo-small-high-res.png" alt="arise-logo"></img>
-                
-                <div className="header_menubar">
-                    <button>Home</button>
-                    <button>Archive</button>
-                    <button>About</button>
-                    <Link to = "/create">  Create Post </Link>
-                </div>
+    const [click, setClick] = useState(false);
+    const [dropdown, setDropdown] = useState(false);
+    
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
 
-            </div>
+    const onMouseEnter = () => {
+        if (window.innerWidth < 960) {
+          setDropdown(false);
+        } else {
+          setDropdown(true);
+        }
+      };
+    
+      const onMouseLeave = () => {
+        if (window.innerWidth < 960) {
+          setDropdown(false);
+        } else {
+          setDropdown(false);
+        }
+      };
 
-            <div className="header_right">
-                {/* image source will be changed once database is available */}
-                <img src="https://www.apc.edu.ph/wp-content/uploads/2019/10/03_Seal-of-APC.png" alt="apc-logo"></img>
-
-                <div className="header_menubar">
-                    <button>My Profile</button>
-                </div>
-
-            </div>
-
+    return (
+        <>
+      <nav className='navbar'>
+        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+        <img src="https://www.arise.ph/wp-content/uploads/2021/10/arise-new-logo-small-high-res.png" alt="arise-logo"></img>
+        </Link>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
-    )
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Archive
+            </Link>
+          </li>
+          <li
+            className='nav-item'
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link
+              to='/profile'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              My Profile <i className='fas fa-caret-down' />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+        </ul>
+      </nav>
+        </>
+    );
     
 }
 
---
+++ src/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+      title: 'Edit Profile',
+      path: '/create',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'Create Post',
+      path: '/create',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'Sign Out',
+      path: '/signout',
+      cName: 'dropdown-link'
+    },
+  ];
