d352871d6f29ad1ecedb88335ff6a6235c8886d1
Pat <ggl.pdmm@gmail.com>
2021-11-18 16:04:47 +0800

    edit calendar with dummy data

--- src/calendar/Calendar.js
+++ src/calendar/Calendar.js
@@ -22,7 +22,33 @@ const localizer = dateFnsLocalizer({
     locales,
 });
 
-const  events = [];
+const  events = [
+
+    {
+        title: "Fire Prevention Month",
+        start: new Date(2021, 10, 1),
+        end: new Date(2021, 10, 1),
+    },
+
+    {
+        title: "Fire Drill",
+        start: new Date(2021, 10, 20),
+        end: new Date(2021, 10, 22),
+    },
+
+    {
+        title: "Fire Awareness Webinar",
+        start: new Date(2021, 10, 25),
+        end: new Date(2021, 10, 27),
+    },
+    
+    {
+        title: "ARISE Conference",
+        start: new Date(2021, 11, 15),
+        end: new Date(2021, 11, 20),
+    },
+
+];
 const CalendarScreen = () => {
     const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
     const [allEvents, setAllEvents] = useState(events);
@@ -33,15 +59,6 @@ const CalendarScreen = () => {
 
     return(
         <div>
-            {/* <h1>Calendar</h1>
-            <h2>Add New Event</h2> */}
-            <div className="event_handler">
-                <input type="text" placeholder="Add Title" value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-                <DatePicker placeholderText="Start Date" selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
-                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
-                <button onClick={handleAddEvent}>Add Event</button>
-            </div>
-
             <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
         </div>
     )
